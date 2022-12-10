3123c9ed759fe653f76df201ead6622c6fe3b52a
Vladimir <vova.okhotin@mail.ru>
2022-12-10 14:29:06 +0800

    hs

--- Example_hard_stat/Program.cs
+++ Example_hard_stat/Program.cs
@@ -6,29 +6,7 @@ int[] CreateArray(int size) // создание массива и заполне
         array[i] = new Random().Next(-99, 100);
     return array;
 }
-void MaxMin(int[]array)
-{
-    int max=array[0];
-    int min=array[0];
-    for (int i=0;i<array.Length; i++)
-    {
-    if (array[i]>max) {max=array[i]; max_ind = i; Console.Write($"{array[i]} ");}
-    else if (array[i]<=min){min=array[i]; min_ind = i; Console.Write($"{array[i]} ");}
-    else Console.Write($"{array[i] } ");
-    }
-}
-
-void Middle(int[]array)
-{
-    int sum=0;
-    int mid=0;
-    foreach (int item in array)
-    {
-        sum+=el;
-    }
-    mid=sum/array.Length;
-}
-void Arrange(int[]array)
+int [] Arrange(int[]array)// Упорядочивание массива
 {
     for (int i = 0; i < array.Length-1; i++)
     {
@@ -46,21 +24,66 @@ void Arrange(int[]array)
         array[i]=array[minPosition];
         array[minPosition] = temporary;
     }
+    return array;
 }
-void Median(int[]array)
+void PrintArray(int[] array) // вывод массива оператором foreach
 {
-    int med = 0;
-    if (array.Length%2!=0) med=array[array.Length/2+1];
-    else med=(array[array.Length/2]+array[array.Length/2+1])/2;
+    foreach (int el in array)
+        Console.Write($"{el} ");
+    Console.WriteLine();
 }
-void Info(int med, int mid, int max, int min, int max_ind, int min_ind)
+int[] MaxMin(int[]array,int[] info)// Нахождение минималььного  и максимального элемента и их индексов
 {
-    int []array=new int[6];
-    array[0]=min; 
-    array[1]=min_ind;Console.WriteLine($"Минимальное элемент массива {min} находящийся в {min_ind} индексе");
-    array[2]=max;
-    array[3]=max_ind;Console.WriteLine($"Максимальный элемент массива {max} находящийся в {max_ind} индексе");
-    array[4]=mid;Console.WriteLine($"Среднее арифметическое значение массива равно {mid}");
-    array[5]=med;Console.WriteLine($"Среднее арифметическое значение массива равно {mid}");
+    int max =0;
+    int min =0;
+    int max_ind=0;
+    int min_ind=0;
+    for (int i=0;i<array.Length; i++)
+    {
+    if (array[i]>max) {max=array[i]; max_ind = i;}
+    else if (array[i]<=min){min=array[i]; min_ind = i;}
+    }
+    info[0]=max;
+    info[1]=max_ind;
+    info[2]=min;
+    info[3]=min_ind;
+    return info;
 }
-int []array = CreateArray(5);
+int[] Middle(int[]array,int[] info)// Нахождение среднего значения массива
+{
+    int sum=0;
+    var mid=0;
+    foreach (int el in array)
+    {
+        sum+=el;
+    }
+    mid=sum/array.Length;
+    info[4]=mid;
+    return info;
+}
+int[] Median(int[]array,int[] info)//Нахождение медианного значения массива
+{
+    var med = 0;
+    var i_med=array.Length-1;
+    if (array.Length%2!=0) med=array[i_med/2];
+    else med=(array[i_med/2]+array[i_med/2+1])/2;
+    info[5]=med;
+    return info;
+}
+void InfoInput(int[]iar,int[]ar1, int[]ar2) // Формирование массива с инфо и вывод сей
+{
+    Console.WriteLine($"В данном массиве: максимальным элементом является число {iar[0]}, находящееся в {iar[1]} индексе; минимальным элементом {iar[2]}, находщееся в {iar[3]} индексе; среднее арифметическое равно: {iar[4]}; медиальным значением массива является {iar[5]} ");
+}
+Console.WriteLine("Введите размерность массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int []array1=CreateArray(size);
+int[]info0=new int[6];// создали пустой массив для заполнения его информацией
+Console.Write("Созданный массив из рандомных чисел: ");
+PrintArray(array1);
+int[]info1=MaxMin(array1,info0);
+int[]info2=Middle(array1,info1);
+int []array2=Arrange(array1);
+int[]info3=Median(array2,info2);
+InfoInput(info3, array1, array2);
+Console.Write("** Этот же массив выставленный по порядку: ");
+PrintArray(array2);
